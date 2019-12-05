883898c861ff079fe7c70d5c6da9389e7a90a601
quentinchampenois <quentin.champenois@eemi.com>
2019-12-05 18:34:34 +0100

    After email confirmation redirect to the socio demographic authorization form

--- lib/decidim/extends/controllers/confirmations_controller_extend.rb
+++ lib/decidim/extends/controllers/confirmations_controller_extend.rb
@@ -13,14 +13,14 @@ module ConfirmationsControllerExtend
       sign_in(resource) unless user_signed_in? # In case you want to sign in the user
 
       if first_login_and_not_authorized?(resource)
-        decidim_verifications.authorizations_path
+        decidim_verifications.new_authorization_path(action: :new, handler: :socio_demographic_authorization_handler)
       else
         super
       end
     end
 
     def first_login_and_not_authorized?(user)
-      user.is_a?(Decidim::User) && user.sign_in_count == 1 && Decidim::Verifications.workflows.any? && user.verifiable?
+      user.is_a?(Decidim::User) && user.sign_in_count == 1 && Decidim::Verifications.find_workflow_manifest(:socio_demographic_authorization_handler) && user.verifiable?
     end
   end
 end

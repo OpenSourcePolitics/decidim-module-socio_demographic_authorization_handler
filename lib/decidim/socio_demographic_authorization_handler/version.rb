e106adac655392c759ad4681130dcfdee7497316
MoretS <s.moret@ecole-ipssi.net>
2021-09-21 14:33:21 +0200

    Bump to 0.23.6

--- lib/decidim/socio_demographic_authorization_handler/version.rb
+++ lib/decidim/socio_demographic_authorization_handler/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-meetings version.
   module SocioDemographicAuthorizationHandler
     def self.version
-      "0.23.1"
+      "0.23.6"
     end
 
     def self.decidim_version
-      "v0.23.1"
+      "v0.23.6"
     end
   end
 end
